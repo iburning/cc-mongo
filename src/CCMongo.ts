93cfde6cbd070644b00e50899fd3a4af7afcccf8
burning <iburning@live.cn>
2023-06-06 14:25:34 +0800

    add default options for connecting

--- src/CCMongo.ts
+++ src/CCMongo.ts
@@ -11,29 +11,40 @@ export default class CCMongo {
   private uri!: string;
   private dbName!: string;
   private db!: Db;
+  private defaultOptions = {
+    // 于启用新的URL解析器
+    useNewUrlParser: true, 
+    // 启用MongoDB 4.0及更高版本中引入的统一拓扑（Unified Topology）功能
+    // 启用统一拓扑功能可以提高代码的可维护性和可扩展性
+    // 因为它允许您使用相同的代码来访问不同类型的MongoDB实例
+    useUnifiedTopology: true
+  };
+  private options: any
 
-  constructor(uri: string, dbName: string) {
+  constructor(uri: string, dbName: string, options: any) {
     if (CCMongo.instance) {
       return CCMongo.instance;
     }
 
     this.uri = uri;
     this.dbName = dbName;
+    this.options = options;
     CCMongo.instance = this;
   }
 
   public init() {
     return new Promise<Db>((resolve, reject) => {
-      MongoClient.connect(this.uri)
-        .then((client) => {
-          console.log('MongoDB connected successfully!', this.uri, this.dbName);
-          this.db = client.db(this.dbName);
-          resolve(this.db);
-        })
-        .catch((err) => {
-          console.error(`Error connecting to MongoDB: ${err}`);
-          reject(err);
-        });
+      MongoClient.connect(this.uri, {
+        ...this.defaultOptions,
+        ...this.options
+      }).then((client) => {
+        console.log('MongoDB connected successfully!', this.uri, this.dbName);
+        this.db = client.db(this.dbName);
+        resolve(this.db);
+      }).catch((err) => {
+        console.error(`Error connecting to MongoDB: ${err}`);
+        reject(err);
+      });
     });
   }
 
